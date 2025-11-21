415d0c5405e26e65f9db0dcbcfe226fe7385c3d7
margaretpalma <mpalma1496@gmail.com>
2025-11-21 11:11:30 -0500

    commit 1

---
+++ 01_new_supplier.sql
@@ -0,0 +1,3 @@
+use northwind;
+insert into suppliers (SupplierID, CompanyName, ContactName, ContactTitle, Address, City, Region, PostalCode, Country, Phone, Fax, HomePage)
+values (30, 'Redbull Inc', 'Ezra Blue', 'CEO', '111 River Ln', 'Charlotte', 'NC', '28210', 'USA', '(336)777-7777', '(250)855-1025', "http://www.sunrisefinefoods.com");
